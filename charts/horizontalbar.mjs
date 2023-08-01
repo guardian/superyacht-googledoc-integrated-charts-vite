661af2ac668ff2e7fa76a59751dfbe3c4d0f4bbf
Andy Ball <work@andyball.info>
2023-08-01 13:18:03 +1000

    Update how the x column in the charts/horizontalbar is set (Before it loads the module)

--- charts/horizontalbar.mjs
+++ charts/horizontalbar.mjs
@@ -113,7 +113,7 @@ export default class Horizontalbar {
 
     d3.select("#graphicContainer svg").remove()
 
-    xColumn = stackedhorizontal[0]
+    //xColumn = stackedhorizontal[0] // Changed this so it is set by default before this module is loaded
 
     stackedhorizontal = stackedhorizontal.filter(d => d != 'Color')
 
@@ -406,8 +406,6 @@ export default class Horizontalbar {
       .text((d) => numberFormat(d.Total) + suffix)
 
     } else {
-
-      console.log("Sup bro")
 
       layer
       .selectAll(".barNumber")
