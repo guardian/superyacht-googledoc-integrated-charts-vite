33cf9ae412314f2ce914d7a099a152d768406da3
Andy Ball <work@andyball.info>
2023-11-13 11:31:15 +1100

    Update default colour sets

--- charts/constants/colors.mjs
+++ charts/constants/colors.mjs
@@ -1,5 +1,16 @@
 export default {
-  guardian: ["#CC0A11","#046DA1","#f28e2c","#76b7b2","#59a14f","#edc949","#af7aa1","#ff9da7","#9c755f","#bab0ab","#000000"
+  guardian: [
+    "#CC0A11",
+    "#046DA1",
+    "#f28e2c",
+    "#76b7b2",
+    "#59a14f",
+    "#edc949",
+    "#af7aa1",
+    "#ff9da7",
+    "#9c755f",
+    "#bab0ab",
+    "#000000"
   ],
   guardian2: [
     "rgb(204, 10, 17)",
@@ -10,6 +21,24 @@ export default {
     "rgb(179, 179, 180)",
     "rgb(128, 128, 128)",
     "#000000"
-    
+  ],
+  darkmode: [
+    "#ffd700",
+    "#ea5f94",
+    "#13F4EF",
+    "#fa8775",
+    "#cd34b5",
+    "#9d02d7",
+    "#0000ff"
+  ],
+  "the-crunch":[
+    "#ffd700",
+    "#ea5f94",
+    "#13F4EF",
+    "#fa8775",
+    "#cd34b5",
+    "#9d02d7",
+    "#0000ff"
   ]
+
 }
