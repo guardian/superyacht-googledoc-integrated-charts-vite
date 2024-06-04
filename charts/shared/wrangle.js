9b8e330e72faa234f238193db5ad27a9ea80e31f
Andy Ball <work@andyball.info>
2024-06-04 10:24:31 +1000

    Updates

--- charts/shared/wrangle.js
+++ charts/shared/wrangle.js
@@ -3,342 +3,217 @@ import { schema } from './schema';
 import dataTools from "../dataTools"
 import ColorScale from "./colorscale"
 
-/*
-/
-/ This is the data preprocessing function.
-/ The ultimate goal is ...
-/
-*/
+import { max as d3Max } from 'd3-array';
+import { timeParse as d3TimeParse, timeFormat as d3TimeFormat } from 'd3-time-format';
 
+// Define constants for frequently used values
+const SPECIAL_KEYS = ["template", "options", "chartId"];
+const CONVERT_TO_NUMBER = ['marginleft','marginright','margintop','marginbottom', 'numCols', 'height', 'maxHeight', 'opacity'];
+const SCALE_KEYS = ['xScale', 'yScale', 'zScale'];
+const SCALE_TYPES = ['scaleLinear', 'scaleSqrt', 'scalePow', 'scaleLog', 'scaleTime', 'scaleSequential', 'scaleQuantize', 'scaleThreshold', 'scaleOrdinal', 'scaleBand'];
+const BOOLEAN_KEYS = ['enableShowMore','aria', 'forceCentre','enableSearch', 'enableSort', 'enableScroll', 'zero_line_x', 'zero_line_y', 'lineLabelling', "autoSort", "scaleByAllMax", "hideKey", "beeswarm", "invertY", "breaks", "zeroLineX", "zeroLineY"];
+const AXIS_TYPES = ['stackedbar', 'linechart', 'smallmultiples', 'stackedarea', 'bubble', 'scatterplot', 'lollipop', 'verticalbar'];
+
+/**
+ * Data preprocessing function
+ * @param {Object} data - The input data object.
+ * @param {Object} chart - The chart configuration object.
+ * @returns {Object} - The processed settings object.
+ */
 export async function wrangle(data, chart) {
+  const settings = initializeSettings(data);
 
-  let keys = Object.keys(data)
+  if (data.columns && data.data) {
+    const columns = data.columns.length ? data.columns : await schema(data.data);
+    settings.columns = columns;
+    settings.columnMap = new Map(columns.map(d => [d.column, d]));
+    console.log("---- New schema info ----", columns, "---- Ends ----");
+  }
 
-  let settings = {}
+  processSettings(settings);
 
-  settings.modules = {}
-
-  settings.height = 0
-
-  settings.width = 0
-
-  settings.svgWidth = 0
-  settings.svgHeight = 0
-  settings.featuresWidth = 0
-  settings.featuresHeight = 0
-  settings.isMobile = null
-
-  settings.colors = null
-
-  settings.datum = []
-
-
-  for (let key of keys) {
-
-    if (key == "template" || key == "options" || key == "chartId" ) {
-
-      let special_keys = Object.keys(data[key][0])
-
-      for (let special_key of special_keys) {
-
-        settings[special_key.replace('-', '')] = data[key][0][special_key]
-      
+  if (settings.dropdown?.length) {
+    settings.dropdown = settings.dropdown.map(dropdown => {
+      const mergedDropdown = merge({ label: "", values: "", tooltip: "", colours: "", data: "" }, dropdown);
+      if (!mergedDropdown.label && mergedDropdown.data) {
+        mergedDropdown.label = mergedDropdown.data;
+        if (!mergedDropdown.values) {
+          mergedDropdown.values = mergedDropdown.data;
+        }
       }
+      return mergedDropdown;
+    });
+  }
 
+  updateAxisSettings(chart, settings);
+  applyChartSettings(chart, settings);
+
+  if (settings.type === "table" && data.userkey) {
+    processTableGraphics(data.userkey, settings, data);
+  }
+
+  if (contains(AXIS_TYPES, settings.type)) {
+    settings.xFormat = xFormatting(settings);
+  }
+
+  processDateSettings(settings);
+
+  console.log(Object.keys(settings), settings);
+  return settings;
+}
+
+/**
+ * Initializes settings from the input data.
+ * @param {Object} data - The input data object.
+ * @returns {Object} - The initialized settings object.
+ */
+function initializeSettings(data) {
+  const settings = {
+    modules: {},
+    height: 0,
+    width: 0,
+    svgWidth: 0,
+    svgHeight: 0,
+    featuresWidth: 0,
+    featuresHeight: 0,
+    isMobile: null,
+    colors: null,
+    datum: []
+  };
+
+  Object.keys(data).forEach(key => {
+    if (SPECIAL_KEYS.includes(key)) {
+      Object.entries(data[key][0]).forEach(([specialKey, value]) => {
+        settings[specialKey.replace('-', '')] = value;
+      });
     } else {
-
-      if (key == 'key') {
-
-        settings.userkey = data[key]
-
+      settings[key === 'key' ? 'userkey' : key] = data[key];
+      if (key === 'data') {
+        processData(data[key], settings);
       }
+    }
+  });
 
-      if (key == 'data') {
+  return settings;
+}
 
-        let dataKeys = Object.keys(data[key][0])
+/**
+ * Processes the data and updates settings.
+ * @param {Array} data - The data array.
+ * @param {Object} settings - The settings object to be updated.
+ */
+function processData(data, settings) {
+  const dataKeys = Object.keys(data[0]);
+  settings.keys = dataKeys;
 
-        settings.keys = dataKeys
-
-        for (let row of data[key]) {
-
-          for (let cell of dataKeys) {
-
-            if (row[cell] === "0" || row[cell] === "0.0") {
-
-              row[cell] = 0
-
-            }
-
-            if (row[cell] === "") {
-
-              row[cell] = null
-
-            }
-
-            /*
-            / Handle numbers with commas
-            */
-
-            if (row[cell] && typeof row[cell] === "string"  && row[cell].includes(",")) {
-
-              if (!isNaN(row[cell].replace(/,/g, ""))) {
-
-                row[cell] = +row[cell].replace(/,/g, "")
-
-              }
-
-            }
-
-            row[cell] = (typeof row[cell] === "string" && !isNaN(row[cell])) ? +row[cell] : row[cell]
-
-          }
-        
-        }
-
+  data.forEach(row => {
+    dataKeys.forEach(cell => {
+      if (row[cell] === "0" || row[cell] === "0.0") {
+        row[cell] = 0;
       }
-
-      settings[key] = data[key]
-
-    }
-
-  }
-
-  if (keys.includes("columns") && keys.includes("data")) {
-
-    let columns = data["columns"]
-
-    if (columns.length == 0) {
-
-      columns = await schema(data["data"])
-
-    }
-
-    settings["columns"] = columns
-
-    settings["columnMap"] = new Map(columns.map(d => [ d.column, d]));
-    
-    console.log("---- New schema info ----")
-    console.log(columns)
-    console.log("---- Ends ----")
-
-  }
-
-
-  let curated = Object.keys(settings)
-
-  for (const setting of curated) {
-
-    // Convert strings to numbers
-
-    if (contains(['marginleft','marginright','margintop','marginbottom', 'numCols', 'height', 'maxHeight', 'opacity'], setting)) {
-
-      settings[setting] = (isNaN(settings[setting])) ? settings[setting] : +settings[setting]
-
-    }
-
-    if (contains(['xMin','xMax','yMin','yMax', 'zMin', 'zMax'], setting)) {
-
-      if (settings[setting] != "") {
-
-        settings[setting] = (isNaN(settings[setting])) ? settings[setting] : +settings[setting]
-
-      }      
-
-    }
-
-    // Convert booleans
-
-    if (contains(['enableShowMore','aria', 'forceCentre','enableSearch', 'enableSort', 'enableScroll', 'zero_line_x', 'zero_line_y', 'lineLabelling', "autoSort", "scaleByAllMax", "hideKey", "beeswarm", "invertY", "breaks", "zeroLineX", "zeroLineY"], setting)) {
-
-      settings[setting] = (settings[setting].toLowerCase() == 'false') ? false : true
-
-    }
-
-    // Check that d3 scales are valid
-
-    if (contains(['xScale, yScale, zScale'], setting)) {
-
-      settings[setting] = (contains(['scaleLinear','scaleSqrt','scalePow','scaleLog','scaleTime','scaleSequential','scaleQuantize','scaleThreshold','scaleOrdinal','scaleBand'], settings[setting])) ? settings[setting] : 'scaleLinear'
-
-    }
-
-  }
-
-  /*
-  / Add any missing stuff to the dropdown object
-  */
-
-  if (settings.dropdown) {
-
-    if (settings.dropdown.length > 0) {
-
-      let drops = []
-
-      for (let dropdown of settings.dropdown) {
-
-        dropdown = merge({ "label" : "" , "values" : "" , "tooltip" : "", "colours" : "", "data" : "" }, dropdown)
-
-        if (dropdown.label == "" && dropdown.data != "") {
-
-          dropdown.label = dropdown.data
-
-          if (dropdown.values == "") {
-
-            dropdown.values = dropdown.data
-
-          }
-
-        }
-
-        drops.push(dropdown)
-      
+      if (row[cell] === "") {
+        row[cell] = null;
       }
+      if (typeof row[cell] === "string" && row[cell].includes(",") && !isNaN(row[cell].replace(/,/g, ""))) {
+        row[cell] = +row[cell].replace(/,/g, "");
+      }
+      row[cell] = (typeof row[cell] === "string" && !isNaN(row[cell])) ? +row[cell] : row[cell];
+    });
+  });
+}
 
-      settings.dropdown = drops
-
+/**
+ * Processes settings by converting values to appropriate types.
+ * @param {Object} settings - The settings object to be processed.
+ */
+function processSettings(settings) {
+  Object.keys(settings).forEach(setting => {
+    if (contains(CONVERT_TO_NUMBER, setting)) {
+      settings[setting] = isNaN(settings[setting]) ? settings[setting] : +settings[setting];
     }
 
-  }
+    if (contains(['xMin', 'xMax', 'yMin', 'yMax', 'zMin', 'zMax'], setting) && settings[setting] !== "") {
+      settings[setting] = isNaN(settings[setting]) ? settings[setting] : +settings[setting];
+    }
 
+    if (contains(BOOLEAN_KEYS, setting)) {
+      settings[setting] = settings[setting].toLowerCase() !== 'false';
+    }
 
-  /*
-  / Set axis values if they are not specified
-  */
+    if (contains(SCALE_KEYS, setting)) {
+      settings[setting] = contains(SCALE_TYPES, settings[setting]) ? settings[setting] : 'scaleLinear';
+    }
+  });
+}
 
+/**
+ * Updates axis settings based on the chart configuration.
+ * @param {Object} chart - The chart configuration object.
+ * @param {Object} settings - The settings object to be updated.
+ */
+function updateAxisSettings(chart, settings) {
   if (chart.axis) {
-
-    for (const setting of chart.axis) {
-
-      if (settings[setting.name] == "") {
-
-        //console.log(setting.name)
-
-        settings[setting.name] = settings.keys[setting.default]
-
+    chart.axis.forEach(setting => {
+      if (!settings[setting.name]) {
+        settings[setting.name] = settings.keys[setting.default];
       }
-    
-    }
-
+    });
   }
+}
 
-
+/**
+ * Applies additional chart settings.
+ * @param {Object} chart - The chart configuration object.
+ * @param {Object} settings - The settings object to be updated.
+ */
+function applyChartSettings(chart, settings) {
   if (chart.settings) {
+    chart.settings.forEach(setting => {
+      settings[setting.name] = dataTools[setting.name](settings);
+    });
+  }
+}
 
-    for (const setting of chart.settings) {
+/**
+ * Processes table graphics for the settings object.
+ * @param {Array} userKeys - The user keys array.
+ * @param {Object} settings - The settings object to be updated.
+ * @param {Object} data - The input data object.
+ */
+function processTableGraphics(userKeys, settings, data) {
+  userKeys.forEach(key => {
+    key.format = key.format || [];
+    if (contains('bar', key.format)) {
+      const range = data.data.map(item => item[key.key]);
+      const max = d3Max(range);
+      const colors = key.colours?.includes(",") ? key.colours.split(',') : [key.colours || "red"];
+      const domain = key.values?.includes(",") ? key.values.split(',') : [key.values || ""];
+      const scale = key.scale || 'Linear';
 
-      settings[setting.name] = dataTools[setting.name](settings)
-    
+      key.graphics = {
+        type: "bar",
+        max: max,
+        colour: new ColorScale({
+          type: capitalizeFirstLetter(scale),
+          domain: domain,
+          colors: colors
+        })
+      };
     }
+  });
+}
 
+/**
+ * Processes date settings for the settings object.
+ * @param {Object} settings - The settings object to be updated.
+ */
+function processDateSettings(settings) {
+  if (settings.dateFormat) {
+    console.log(`Set date format to ${settings.dateFormat}`);
+    settings.parseTime = settings.dateFormat ? d3TimeParse(settings.dateFormat) : null;
   }
 
-  /*
-  / If this is a table with graphics
-  / format the graphics data
-  */
-
-
-
-
-  if (settings["type"] == "table") {
-
-    if (data.userkey) {
-
-      for (const key of data.userkey) {
-
-        key.format = (key.format) ? key.format : [] ;
-
-        if (contains('bar', key.format)) {
-
-          let range = data.data.map(item => item[key.key])
-
-          let max = d3.max(range)
-
-          let colours = (key.colours && key.colours != "" && key.colours.includes(",")) ? key.colours.split(',') :
-          (key.colours && key.colours != "") ? [ key.colours ] : ["red"]
-
-          let domain = (key.values && key.values.includes(",")) ? key.values.split(',') :
-          key.values != "" ? [ key.values ] : [];
-
-          let scale = (key.scale) ? key.scale :  'Linear' ;
-
-          // Need to check if scale type is valid... one for later
-
-          let colour  = new ColorScale({
-            type: capitalizeFirstLetter(scale),
-            domain: domain,
-            colors: colours
-          })
-
-          key.graphics = { type : "bar", max : max, colour : colour }
-
-        }
-
-      }
-
-    }
-
+  if (settings.xAxisDateFormat) {
+    settings.xAxisDateFormat = settings.xAxisDateFormat ? d3TimeFormat(settings.xAxisDateFormat) : d3TimeFormat("%d %b '%y");
   }
-
-  /*
-  / If the chart type includes an xColumn 
-  / check if it is a date, a string
-  */
-
-  if (contains(['stackedbar', 'linechart', 'smallmultiples', 'stackedarea', 'bubble', 'scatterplot', 'lollipop', 'verticalbar'], settings["type"])) {
-
-    settings["xFormat"] = xFormatting(settings)
-
-    //console.log(settings["xFormat"])
-
-  }
-
-  /*
-  / change dateFormat var to xFormat... types to be datastring, number, string
-  */
-
-  if (settings["dateFormat"]) {
-
-    console.log(`Set date format to ${settings["dateFormat"]}`)
-
-    /*
-    / Handle dates - set parseTime if it is a date... 
-    / replaced all instances of dateParse with parseTime
-    / Add date detection for xColumn to linechart, stacked bar, stackedarea, smallmultiples... 
-    / use xFormat variable (req dateFormat and dateParse, xAxisDateFormat needs a default)
-    */
-
-    settings.parseTime = (settings["dateFormat"]!="") ? d3.timeParse(settings["dateFormat"]) : null
-
-
-  }
-
-  /*
-  / Set xAxisDateFormat
-  */
-
-  if (settings["xAxisDateFormat"]) {
-
-    if (settings["xAxisDateFormat"] != "") {
-
-      settings["xAxisDateFormat"] = d3.timeFormat(settings["xAxisDateFormat"])
-
-    } else {
-
-      if (settings["dateParse"]) {
-
-        settings["xAxisDateFormat"] = d3.timeFormat("%d %b '%y")
-
-      }
-
-    }
-
-  }
-
-  // console.log(Object.keys(settings))
-
-  // console.log(settings)
-
-  return settings
-
 }
